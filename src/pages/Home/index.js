a3feeb8b005e0825b74ffbe08eda03be6fc5756a
Ruben E Acosta <eldragondera1@gmail.com>
2020-03-22 14:31:40 -0500

    home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,60 @@
-import React from 'react';
-import { Container, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import {
+  Container,
+  Card,
+  Grid,
+  Typography,
+  TextField,
+  Button
+} from '@material-ui/core';
+
+import { MovieIcon } from '../../icons';
+import styles from './styles';
 
 const Home = () => {
+  const [search, setSearch] = useState('');
+  const clases = styles();
+
+  const handleSearch = () => {
+
+  };
 
   return (
-    <Container>
-      <Typography variant='h4'>
-        HOLA
-      </Typography>
+    <Container className={clases.container}>
+      <Card className={clases.cardContainer}>
+        <Grid container className={clases.titleGridContainer}>
+          <Grid>
+            <Typography className={clases.title}>Bienvenido</Typography>
+          </Grid>
+          <Grid>
+            <MovieIcon className={clases.movieIcon} />
+          </Grid>
+        </Grid>
+
+        <TextField
+          className={clases.textFieldSearch}
+          value={search}
+          placeholder='Buscar'
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <Grid className={clases.buttonsContainer}>
+          <Button
+            variant='contained'
+            onClick={() => setSearch('')}
+          >
+            Limpiar
+          </Button>
+          <Button
+            className={clases.searchButton}
+            variant='contained'
+            color='primary'
+            onClick={handleSearch}
+          >
+            Buscar
+          </Button>
+        </Grid>
+      </Card>
     </Container>
   )
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,44 @@
+import { makeStyles } from '@material-ui/styles';
+
+const centeredObj = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+};
+
+export default makeStyles({
+  container: {
+    height: '100vh',
+    ...centeredObj,
+    flexDirection: 'column',
+  },
+  cardContainer: {
+    ...centeredObj,
+    flexDirection: 'column',
+    width: 400,
+    height: 200,
+    padding: '2rem',
+    paddingTop: '1rem'
+  },
+  title: {
+    fontSize: '3rem',
+    marginBottom: 6
+  },
+  titleGridContainer: {
+    ...centeredObj,
+    justifyContent: 'space-evenly',
+    marginBottom: '1rem'
+  },
+  textFieldSearch: {
+    width: '90%'
+  },
+  buttonsContainer: {
+    marginTop: '0.5rem'
+  },
+  searchButton: {
+    marginLeft: '0.5rem'
+  },
+  movieIcon: {
+    fontSize: '3rem'
+  }
+});
