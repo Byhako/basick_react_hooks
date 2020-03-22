f1713a42c4e747415097599e5ae9927217d030a1
Ruben E Acosta <eldragondera1@gmail.com>
2020-03-22 13:39:08 -0500

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Typography } from '@material-ui/core';
+
+const Home = () => {
+
+  return (
+    <Container>
+      <Typography variant='h4'>
+        HOLA
+      </Typography>
+    </Container>
+  )
+}
+
+export default Home;
